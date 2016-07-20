ad5714a6068e1fd1458a4d8ce03ae2fad4585474
FischerTo <tofischer@uos.de>
2016-07-20 11:37:46 +0200

    change code to be shorter

--- task4/tobas_fischer/sum_prod.rs
+++ task4/tobas_fischer/sum_prod.rs
@@ -1,11 +1,8 @@
+use std::ops::{Add, Mul};
 fn main() {
-
     println!("{}, {}", sf(2, 4).0, sf(2, 4).1)
 }
 
-pub fn sf<T: std::ops::Add + std::ops::Mul + Copy>
-    (x: T,
-     y: T)
-     -> (<T as std::ops::Add>::Output, <T as std::ops::Mul>::Output) {
+pub fn sf<T: Add + Mul + Copy>(x: T, y: T) -> (<T as Add>::Output, <T as Mul>::Output) {
     (x + y, x * y)
 }
